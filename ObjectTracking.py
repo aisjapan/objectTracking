20376fcc789a7132959d25a0e6a3b7d690629059
Shiyu Oguro <35251961+Shiyu-Oguro@users.noreply.github.com>
2018-08-23 09:15:08 +0900

    いらない部分を削った
    ・関数化したり、いらない部分削ったりしました。

--- ObjectTracking.py
+++ ObjectTracking.py
@@ -19,13 +19,21 @@ FRAME_W = 800
 FRAME_H = 600
 
 #読み込むピクチャのファイル名
-FILE_DIST = 1000
+FILE_DIST = 200
 
 HSV_CONV = 0.70 #HSV, 360dankai => 256dankai , 250/360 = 0.7
 #colorRange = [[RedMin],[RedMax],[BlueMin],[BlueMax],[GreenMin],[GreenMax],[PurpleMin][PurpleMax],[YellowMin][YellowMax]]
 colorRange = [[30*HSV_CONV,330*HSV_CONV], [200*HSV_CONV,240*HSV_CONV], [80*HSV_CONV,150*HSV_CONV], [240*HSV_CONV,300*HSV_CONV],[40*HSV_CONV,80*HSV_CONV]]
 
+# 距離導出関数のマジックナンバー (係数,指数)
+COEFFICIENT_DF = 200198
+INDEX_DF = -0.808
 #-----------------------------------------------------------------------------------
+# Loading Image's
+def loading_still_image():
+  image = cv2.imread('C:\\Users\\nct20\\Documents\\GitHub\\objectTracking\\image_input\\'+str(FILE_DIST)+'mm.jpg')
+  return image
+
 #キー処理＆画面終了＆保存
 def key_action(image):
 
@@ -87,65 +95,39 @@ def find_rect_of_target_color(getImage,colorNum):
   for i in range(0,2):
     for contour in contours:
       approx = cv2.convexHull(contour)
-      #rect = cv2.boundingRect(approx)
-
-      rect = cv2.minAreaRect(approx)
-      #box = cv2.boxPoints(rect)
-      #box = np.int0(box)
+      rect = cv2.minAreaRect(approx) #回転を考慮した矩形の導出
       rects.append(np.array(rect))
-      #box.append(np.array(box))
 
   return rects
 
-if __name__ == '__main__':
+def draw_rotation_rectangle(rects,image):
+  #rects [[0],[1]] [[0],[1]] = [[x],[y]] [[w],[h]]
+  rect = tuple(max(rects, key = (lambda x: x[1][0] * x[1][1])))
+  box = cv2.boxPoints(rect)
+  box = np.int0(box)
 
+  image = cv2.drawContours(image,[box],0,(0,0,255),2)
+  print ('w:' + str(rect[1][0]) + ',h:' + str(rect[1][1]))
+  print (str(COEFFICIENT_DF*((rect[1][0]*rect[1][1])**(INDEX_DF))))
+
+if __name__ == '__main__':
   # Loading Image's
-  image = cv2.imread('C:\\Users\\nct20\\Documents\\GitHub\\objectTracking\\image_input\\'+str(FILE_DIST)+'mm.jpg')
-  template = cv2.imread('C:\\Users\\nct20\\Documents\\GitHub\\objectTracking\\image_input\\robot_sample_200mm.jpg',0)
+  image = loading_still_image()
 
   rectsR = []
   rectsB = []
   rectsG = []
-  #print ("Hello!!")
-  image = cut_circle(image)
-  #image = cv2.flip(image,1)
-  #image = frame_image
 
+  # ミラー領域の切り取り
+  image = cut_circle(image)
+
+  # 色領域の検出と矩形の値導出
   rectsR = find_rect_of_target_color(image, 1)
   rectsB = find_rect_of_target_color(image, 2)
   rectsG = find_rect_of_target_color(image, 3)
-  rectsP = find_rect_of_target_color(image, 4)
-  rectsY = find_rect_of_target_color(image, 5)
 
-  if len(rectsR) >0:
-     rectR = max(rectsR, key = (lambda x: x[2] * x[3]))
-     cv2.rectangle(image, tuple(rectR[0:2]), tuple(rectR[0:2] + rectR[2:4]), (0, 0, 255), thickness=2)
   if len(rectsB) >0:
-     rectB = tuple(max(rectsB, key = (lambda x: x[1][0] * x[1][1])))
-     #cv2.rectangle(image, tuple(rectB[0:2]), tuple(rectB[0:2] + rectB[2:4]), (255, 0, 0), thickness=1)
-     #print (rectB)
-     box = cv2.boxPoints(rectB)
-     box = np.int0(box)
-     image = cv2.drawContours(image,[box],0,(0,0,255),2)
-     print ('w:' + str(rectB[1][0]) + ',h:' + str(rectB[1][1]))
-     #print (str(900 - rectB[3]*700/42) + '[mm]')
-     print (str(200198*((rectB[1][0]*rectB[1][1])**(-0.808))))
-     #print (str(880141/87.908*99.936*100/(rectB[1][0]*rectB[1][1])) + '[mm]')
-     #ミラー画像上での距離の導出
-     #print (math.sqrt( ((rectB[0][0] + rectB[1][0]/2) - FRAME_W/2)**2 + ((rectB[0][1] + rectB[1][1]/2) - (FRAME_H/2+10))**2 ))
-     #radiusV = (math.sqrt( (rectB[0][0] - FRAME_W/2)**2 + (rectB[0][1] - (FRAME_H/2+10))**2 ))
-     #print (str(radiusV/194.28394169359444 * 200) + '[mm]')
-
-  #originX,originY = (int)(FRAME_W/2+10), (int)(FRAME_H/2-20)
-  #x, y = rectR[0]+(int)(rectR[2]/2.0), rectR[1]+(int)(rectR[3]/2.0)
-  #cv2.circle(image,(originX,originY), 20, (0,0,0), -1)
-
-  # calc angle
-  #angle = calcAngle(originX,originY,x,y)
-  #print("Angle = ", angle)
-  # calc distance
-  #distance = calcDistance(originX,originY,x,y)
-  #print("Distance = ", distance)
+     draw_rotation_rectangle(rectsB,image)
 
   cv2.imshow('Recognition Now.', image)
   key_action(image)
